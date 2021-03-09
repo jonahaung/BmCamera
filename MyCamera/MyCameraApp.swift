0196774ebf6a4dde3eb501dedf7f3870d135711e
aungkomin <jonahaung@gmail.com>
2021-03-09 15:27:06 +0800

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Aung Ko Min on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
